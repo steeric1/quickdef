5034d2e6e888a7668b1d521cf099128c2cf86641
Nikko Kujanpää <nikko.kujanpaa@gmail.com>
2022-06-27 21:40:21 +0300

    very basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    for arg in env::args().skip(1) {
+        let body = reqwest::blocking::get(format!(
+            "https://api.dictionaryapi.dev/api/v2/entries/en/{}",
+            arg
+        ))
+        .unwrap()
+        .text()
+        .unwrap();
+
+        println!("{}", body);
+    }
 }
